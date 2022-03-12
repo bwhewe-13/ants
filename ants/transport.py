fd2773ce64b7ec6013007c1f8a3e0858fe45d505
bwhewe-13 <ben.whewell@engineer.com>
2022-03-11 18:59:33 -0700

    Added save and graph options to transport

--- ants/transport.py
+++ ants/transport.py
@@ -23,6 +23,8 @@ class Transport:
         self.input_file = input_file
         self._read_input()
         self._generate_medium_obj()
+        self._generate_materials_obj()
+        self._generate_cross_section()
 
     def _read_input(self):
         self.problem = {}
@@ -63,6 +65,7 @@ class Transport:
             cell_width_x = medium_width_x / cells_x
         else:
             cell_width_x = float(self.problem.get("CELL WIDTH X"))
+        self.cell_width_x = cell_width_x
         angles_x = int(self.problem.get("ANGLES X DIRECTION"))
         self.medium_obj = MediumX(cells_x, cell_width_x, angles_x, xbounds)
         self.medium_obj.add_external_source(self.problem.get("EXTERNAL SOURCE"))
@@ -81,7 +84,8 @@ class Transport:
         for value in self.materials_obj.p_sources.values():
             locations.append(value[0])
             point_source.append(value[1])
-        return np.array(locations), np.array(point_source)
+        self.point_source_locs = np.array(locations)
+        self.point_sources = np.array(point_source)
     
     def _generate_cross_section(self):
         map_obj = os.path.join(self.problem.get("PATH","."), \
@@ -90,6 +94,7 @@ class Transport:
         if int(self.problem.get("CELLS X")) != map_obj.cells_x:
             map_obj.adjust_widths(int(self.problem.get("CELLS X")))
         self.medium_map = map_obj.map_x.astype(int)
+        self.medium_map_key = map_obj.map_key
         reversed_key = {v: k for k, v in map_obj.map_key.items()}
         total = []
         scatter = []
@@ -99,54 +104,191 @@ class Transport:
             total.append(self.materials_obj.data[map_material][0])
             scatter.append(self.materials_obj.data[map_material][1])
             fission.append(self.materials_obj.data[map_material][2])
-        return np.array(total), np.array(scatter), np.array(fission)
+        self.xs_total = np.array(total)
+        self.xs_scatter = np.array(scatter)
+        self.xs_fission = np.array(fission)
 
     def run(self):
-        point_source_locs, point_sources = self._generate_materials_obj()
-        xs_total, xs_scatter, xs_fission = self._generate_cross_section()
         if self.problem.get("TIME DISCRETE") == "backward-euler":
-            scalar, angular = backward_euler(self.medium_map, 
-                                    xs_total, xs_scatter, xs_fission, \
+            scalar, angular = backward_euler(self.medium_map, \
+                                    self.xs_total, \
+                                    self.xs_scatter, \
+                                    self.xs_fission, \
                                     self.medium_obj.ex_source, \
-                                    point_source_locs, point_sources, \
+                                    self.point_source_locs, \
+                                    self.point_sources, \
                                     self.medium_obj.spatial_coef_x, \
                                     self.medium_obj.weight, \
                                     self.materials_obj.velocity, \
                                     int(self.problem.get("TIME STEPS")), \
                                     float(self.problem.get("TIME STEP SIZE")), \
                                     spatial=self.problem.get("SPACE DISCRETE"))
+        if int(self.problem.get("TIME STEPS")) == 0:
+            scalar = np.array(scalar[0])
+        else:
+            scalar = np.array(scalar)
+        self.scalar = scalar
+        self.angular = angular
+        print(self.scalar.shape)
+        print(self.angular.shape)
         return scalar, angular
 
-if __name__ == "__main__":
+    def run_save(self):
+        scalar, angular = self.run()
+        self.save()
+        return scalar, angular
 
-    input_file = 'borderless.inp'
-    test = Transport(input_file)
-    _, psi = test.run()
-    mu_x = test.medium_obj.mu_x
+    def run_graph(self):
+        scalar, angular = self.run()
+        self.graph()
+        return scalar, angular
+        
+    def run_graph_save(self):
+        scalar, angular = self.run()
+        self.save()
+        self.graph()
+        return scalar, angular
 
-    medium_width = 1.
-    angles = 2
+    def _generate_file_name(self):
+        file_name = os.path.join(self.problem.get("PATH","."), \
+                            self.problem.get("FILE NAME"))
+        file_number = 0
+        while os.path.exists(file_name + str(file_number) + ".npz"):
+            file_number += 1
+        return file_name + str(file_number)
 
-    cells_x = 20
-    cell_width_x = medium_width / cells_x
+    def save(self):
+        dictionary = {}
+        file_name = self._generate_file_name()
+        # Neutron fluxes
+        dictionary["flux-scalar"] = self.scalar
+        dictionary["flux-angular"] = self.angular
+        # Medium data
+        dictionary["medium-map"] = self.medium_map
+        dictionary["medium-key"] = self.map_obj.map_key
+        # Cross sections
+        dictionary["groups"] = int(self.problem.get("ENERGY GROUPS"))
+        dictionary["xs-total"] = self.xs_total
+        dictionary["xs-scatter"] = self.xs_scatter
+        dictionary["xs-fission"] = self.xs_fission
+        # Spatial Info
+        dictionary["cells-x"] = int(self.problem.get("CELLS X"))
+        dictionary["cell-width-x"] = self.cell_width_x
+        dictionary["spatial-disc"] = self.problem.get("SPACE DISCRETE")
+        # Angular Info
+        dictionary["angles-x"] = int(self.problem.get("ANGLES X DIRECTION"))
+        dictionary["boundary-x"] = [self.problem.get("BOUNDARY (x = 0)"),
+                                    self.problem.get("BOUNDARY (x = X)")]
+        # Time Info
+        dictionary["time-steps"] = int(self.problem.get("TIME STEPS"))
+        dictionary["time-step-size"] \
+                             = float(self.problem.get("TIME STEP SIZE"))
+        dictionary["time-disc"] = self.problem.get("TIME DISCRETE")
+        # Extra
+        dictionary["notes"] = self.problem.get("NOTE")
+        np.savez(file_name, **dictionary)
 
-    xspace = np.linspace(0, medium_width, cells_x+1)
-    xspace = 0.5*(xspace[:-1] + xspace[1:])
+    def change_param(self, name, value):
+        if name in self.problem.keys():
+            self.problem[name] = value
+        else:
+            raise KeyError("Not in Input File Keys\nAvailable Keys:\
+                                    \n{}".format(self.problem.keys()))
+        self._generate_medium_obj()
+        self._generate_materials_obj()
+        self._generate_cross_section()
 
-    def ref01(mu, x):
-        if mu > 0:
-            return np.ones(x.shape)
-        elif mu < 0:
-            return 1 - np.exp((medium_width - x) / mu)
+    def fission_rate_density(self, cross_section):
+        cell_data = np.zeros((int(self.problem.get("CELLS X"))))
+        for cell, mat in enumerate(self.medium_map):
+            cell_data[cell] = np.sum(self.scalar[cell] \
+                                * np.sum(cross_section[mat],axis=1))
+        return cell_data
 
-    # def ref02(mu, x):
-    #     return 0.5 - 0.5 * np.exp((medium_width - x) / mu)
+    def fission_rate(self, cross_section):
+        time_data = np.zeros((int(self.problem.get("TIME STEPS"))))
+        for time_step in range(len(time_data)):
+            time_data[time_step] = np.sum(self.cell_width_x * \
+                               self.fission_rate_density(cross_section))
+        return time_data
 
-    for angle in range(2):
+    def _generate_angular_plot(self, angle):
+        ...
+
+    def _generate_scalar_plot(self):
+        ...
+
+    def _generate_rate_density_plot(self):
+        cells_x = int(self.problem.get("CELLS X"))
+        xspace = np.linspace(0, cells_x * self.cell_width_x, cells_x+1)
+        xspace = 0.5*(xspace[:-1] + xspace[1:])
+        scatter_rate = self.fission_rate_density(self.xs_scatter)
+        fission_rate = self.fission_rate_density(self.xs_fission)
         fig, ax = plt.subplots()
-        ax.plot(xspace, ref01(mu_x[angle], xspace), label='True', c='k', ls='--')
-        ax.plot(xspace, psi[:,angle],'-o', label='Angle {}'.format(angle), alpha=0.6, c='r')
-        ax.grid()
-        ax.legend(loc=0)
+        ax.plot(xspace, scatter_rate, c="b", alpha=0.8)
+        ax.set_title("Scatter Rate Density")
+        ax.set_xlabel("Location (cm)")
+        ax.set_ylabel(r"Scatter Rate Density (cc$^{-1}$ s$^{-1}$)")
+        fig, ax = plt.subplots()
+        ax.plot(xspace, fission_rate, c="r", alpha=0.8)
+        ax.set_title("Fission Rate Density")
+        ax.set_xlabel("Location (cm)")
+        ax.set_ylabel(r"Fission Rate Density (cc$^{-1}$ s$^{-1}$)")
 
-    plt.show()
+    def _generate_rate_plot(self):
+        time_x = int(self.problem.get("TIME STEPS"))
+        time_size = float(self.problem.get("TIME STEP SIZE"))
+        tspace = np.linspace(0, time_x*time_size, time_x, endpoint=False)
+        scatter_rate = self.fission_rate(self.xs_scatter)
+        fission_rate = self.fission_rate(self.xs_fission)
+        fig, ax = plt.subplots()
+        ax.plot(xspace, scatter_rate, c="b", alpha=0.8)
+        ax.set_title("Scatter Rate")
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel(r"Scatter Rate (s$^{-1}$)")
+        fig, ax = plt.subplots()
+        ax.plot(xspace, fission_rate, c="r", alpha=0.8)
+        ax.set_title("Fission Rate")
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel(r"Fission Rate (s$^{-1}$)")
+
+    def graph(self):
+        if int(self.problem.get("TIME STEPS")) > 0:
+            self._generate_rate_plot()
+        else:
+            self._generate_rate_density_plot()
+        plt.show()
+
+# if __name__ == "__main__":
+
+#     input_file = 'borderless.inp'
+#     test = Transport(input_file)
+#     _, psi = test.run()
+#     mu_x = test.medium_obj.mu_x
+
+#     medium_width = 1.
+#     angles = 2
+
+#     cells_x = 20
+#     cell_width_x = medium_width / cells_x
+
+#     xspace = np.linspace(0, medium_width, cells_x+1)
+#     xspace = 0.5*(xspace[:-1] + xspace[1:])
+
+#     def ref01(mu, x):
+#         if mu > 0:
+#             return np.ones(x.shape)
+#         elif mu < 0:
+#             return 1 - np.exp((medium_width - x) / mu)
+
+#     # def ref02(mu, x):
+#     #     return 0.5 - 0.5 * np.exp((medium_width - x) / mu)
+
+#     for angle in range(2):
+#         fig, ax = plt.subplots()
+#         ax.plot(xspace, ref01(mu_x[angle], xspace), label='True', c='k', ls='--')
+#         ax.plot(xspace, psi[:,angle],'-o', label='Angle {}'.format(angle), alpha=0.6, c='r')
+#         ax.grid()
+#         ax.legend(loc=0)
+
+#     plt.show()
