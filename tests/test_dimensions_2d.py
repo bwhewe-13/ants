31e5a669b3cbce228eb3593f955727dedca0b9c3
Ben Whewell <ben.whewell@pm.me>
2023-06-29 17:44:26 -0500

    Updated test_dimensions_2d tests

--- tests/test_dimensions_2d.py
+++ tests/test_dimensions_2d.py
@@ -9,126 +9,124 @@
 #
 ########################################################################
 
-import ants
-from ants.fixed2d import source_iteration as iteration
-from ants.utils import dimensions
-# from tests import tools
-
 import pytest
 import numpy as np
+
+import ants
+from ants.fixed2d import source_iteration
+
+# Path for reference solutions
+PATH = "data/weight_maps_2d/"
+
+def cylinder_01():
+    # Problem parameters
+    cells_x = 50
+    cells_y = 50
+    angles = 4
+    groups = 1
+
+    # Spatial Layout
+    radii = [(0.0, 4.279960)]
+    radius = max(radii)[1]
+
+    delta_x = np.repeat(radius * 2 / cells_x, cells_x)
+    edges_x = np.linspace(0, radius * 2, cells_x+1)
+    centers_x = 0.5 * (edges_x[1:] + edges_x[:-1])
+
+    delta_y = np.repeat(radius * 2 / cells_y, cells_y)
+    edges_y = np.linspace(0, radius * 2, cells_y+1)
+    centers_y = 0.5 * (edges_y[1:] + edges_y[:-1])
+    # Boundary conditions
+    bc_x = [0, 0]
+    bc_y = [0, 0]
+    # Cross Sections
+    xs_total = np.array([[0.32640], [0.0]])
+    xs_scatter = np.array([[[0.225216]], [[0.0]]])
+    xs_fission = np.array([[[2.84*0.0816]], [[0.0]]])
+    # Update cross sections for cylinder
+    weight_map = np.load(PATH + "cylinder_two_material.npy")
+    medium_map, xs_total, xs_scatter, xs_fission, weight_map \
+        = ants.cylinder2d(radii, xs_total, xs_scatter, xs_fission, delta_x, \
+                          delta_y, bc_x, bc_y, weight_map=weight_map)
+    # np.save(PATH + "cylinder_two_material", weight_map)
+    # Collect problem dictionary
+    info = {
+                "cells_x": cells_x,
+                "cells_y": cells_y,
+                "angles": angles, 
+                "groups": groups, 
+                "materials": xs_total.shape[0],
+                "geometry": 1, 
+                "spatial": 2, 
+                "qdim": 1,
+                "bc_x": bc_x,
+                "bcdim_x": 1,
+                "bc_y": bc_y,
+                "bcdim_y": 1
+            }
+    # Collect angles
+    angle_x, angle_y, angle_w = ants.angular_xy(info)
+    # Return pertinent information
+    return xs_total, xs_scatter, xs_fission, medium_map, delta_x, \
+        delta_y, angle_x, angle_y, angle_w, info
 
 
 @pytest.mark.smoke
 @pytest.mark.cylinder2d
 @pytest.mark.dimensions
 def test_dimensions_boundary_x():
-    radii = [(0.0, 4.279960)]
-    radius = max(radii)[1]
-    center = (radius, radius)
-    cells_x = 100
-    cells_y = 100
-    delta_x = np.repeat(radius * 2 / cells_x, cells_x)
-    delta_y = np.repeat(radius * 2 / cells_y, cells_y)
-    # Cross Sections
-    xs_total = np.array([[0.32640], [0.0]])
-    xs_scatter = np.array([[[0.225216]], [[0.0]]])
-    xs_fission = np.array([[[2.84*0.0816]], [[0.0]]])
-    # Update cross sections for cylinder
-    # weight_map = math.monte_carlo_weight_matrix(delta_x, delta_y, \
-    #                                             center, radii)
-    weight_map = np.load("data/weight_map_cylinder_two_material.npy")
-    data = dimensions.cylinder_cross_sections(weight_map, xs_total, \
-                                              xs_scatter, xs_fission, \
-                                              cells_x, cells_y)
-    medium_map, xs_total, xs_scatter, xs_fission = data
-    medium_map = dimensions.expand_cylinder_medium_map(medium_map, [1,2,3,4])
-    # Material Parameters
-    groups = 1
-    angles = 4
-    bc = [0, 0]
-    velocity = np.ones((groups))
-    external = np.zeros((cells_x, cells_y)).flatten()
-    # Set boundary terms
-    boundary_y = np.zeros((2,))
-    params = {"cells_x": cells_x, "cells_y": cells_y, "angles": angles, \
-             "groups": groups, "materials": len(xs_total), "geometry": 1, \
-             "spatial": 2, "qdim": 1, "bc_x": bc, "bcdim_x": 0, \
-             "bc_y": bc, "bcdim_y": 0, "steps": 0, "dt": 0, \
-             "adjoint": False, "angular": False}
-    angle_x, angle_y, angle_w = ants._angle_xy(params)
-    angles = params["angles"]
-    bounds = [np.zeros((2,)), np.zeros((2, cells_y)), \
-              np.zeros((2, cells_y, groups)), \
-              np.zeros((2, cells_y, angles, groups))]
+    # Call original problem
+    xs_total, xs_scatter, xs_fission, medium_map, delta_x, delta_y, \
+        angle_x, angle_y, angle_w, info = cylinder_01()
+    # Set boundary_y and external sources
+    external = np.zeros((info["cells_x"] * info["cells_y"]))
+    info["qdim"] = 1
+    boundary_y = np.zeros((2))
+    info["bcdim_y"] = 1
+    # Different boundary_x dimensions
+    bounds = [np.zeros((2,)), np.zeros((2, info["cells_y"])), \
+              np.zeros((2, info["cells_y"], info["groups"])), \
+              np.zeros((2, info["cells_y"], info["angles"]**2, info["groups"]))]
     flux = []
     for loc in [0, 1]:
         for bcdim_x, boundary_x in enumerate(bounds):
-            boundary_x[loc] = 1
-            params["bcdim_x"] = bcdim_x
-            result = iteration(xs_total, xs_scatter, xs_fission, external, \
-                               boundary_x.flatten(), boundary_y, \
-                               medium_map.flatten(), delta_x, delta_y, \
-                               angle_x, angle_y, angle_w, params)
+            boundary_x[loc] = 1.0
+            info["bcdim_x"] = bcdim_x + 1
+            result = source_iteration(xs_total, xs_scatter, xs_fission, \
+                        external, boundary_x.flatten(), boundary_y, medium_map, \
+                        delta_x, delta_y, angle_x, angle_y, angle_w, info)
             boundary_x *= 0.0
             if loc == 1:
                 flux.append(result[::-1,:,0].flatten())
             else:
                 flux.append(result[:,:,0].flatten())
     for ii in range(len(flux) - 1):
-        assert np.all(np.isclose(flux[ii], flux[ii+1], atol=1e-10))
+        assert np.isclose(flux[ii], flux[ii+1], atol=1e-10).all()
 
 
 @pytest.mark.cylinder2d
 @pytest.mark.dimensions
 def test_dimensions_boundary_y():
-    radii = [(0.0, 4.279960)]
-    radius = max(radii)[1]
-    center = (radius, radius)
-    cells_x = 100
-    cells_y = 100
-    delta_x = np.repeat(radius * 2 / cells_x, cells_x)
-    delta_y = np.repeat(radius * 2 / cells_y, cells_y)
-    # Cross Sections
-    xs_total = np.array([[0.32640], [0.0]])
-    xs_scatter = np.array([[[0.225216]], [[0.0]]])
-    xs_fission = np.array([[[2.84*0.0816]], [[0.0]]])
-    # Update cross sections for cylinder
-    # weight_map = math.monte_carlo_weight_matrix(delta_x, delta_y, \
-    #                                             center, radii)
-    weight_map = np.load("data/weight_map_cylinder_two_material.npy")
-    data = dimensions.cylinder_cross_sections(weight_map, xs_total, \
-                                              xs_scatter, xs_fission, \
-                                              cells_x, cells_y)
-    medium_map, xs_total, xs_scatter, xs_fission = data
-    medium_map = dimensions.expand_cylinder_medium_map(medium_map, [1,2,3,4])
-    # Material Parameters
-    groups = 1
-    angles = 4
-    bc = [0, 0]
-    # angles, angle_x, angle_y, angle_w = ants.calculate_xy_angles(angles, [bc,bc])
-    velocity = np.ones((groups))
-    external = np.zeros((cells_x, cells_y)).flatten()
-    # Set boundary terms
-    boundary_x = np.zeros((2,))
-    params = {"cells_x": cells_x, "cells_y": cells_y, "angles": angles, \
-             "groups": groups, "materials": len(xs_total), "geometry": 1, \
-             "spatial": 2, "qdim": 1, "bc_x": bc, "bcdim_x": 0, \
-             "bc_y": bc, "bcdim_y": 0, "steps": 0, "dt": 0, \
-             "adjoint": False, "angular": False}
-    angle_x, angle_y, angle_w = ants._angle_xy(params)
-    angles = params["angles"]
-    bounds = [np.zeros((2,)), np.zeros((2, cells_y)), \
-              np.zeros((2, cells_y, groups)), \
-              np.zeros((2, cells_y, angles, groups))]
+    # Call original problem
+    xs_total, xs_scatter, xs_fission, medium_map, delta_x, delta_y, \
+        angle_x, angle_y, angle_w, info = cylinder_01()
+    # Set boundary_x and external sources
+    external = np.zeros((info["cells_x"] * info["cells_y"]))
+    info["qdim"] = 1
+    boundary_x = np.zeros((2))
+    info["bcdim_x"] = 1
+    # Different boundary_x dimensions
+    bounds = [np.zeros((2,)), np.zeros((2, info["cells_x"])), \
+              np.zeros((2, info["cells_x"], info["groups"])), \
+              np.zeros((2, info["cells_x"], info["angles"]**2, info["groups"]))]
     flux = []
     for loc in [0, 1]:
         for bcdim_y, boundary_y in enumerate(bounds):
             boundary_y[loc] = 1
-            params["bcdim_y"] = bcdim_y
-            result = iteration(xs_total, xs_scatter, xs_fission, external, \
-                               boundary_x, boundary_y.flatten(), \
-                               medium_map.flatten(), delta_x, delta_y, \
-                               angle_x, angle_y, angle_w, params)
+            info["bcdim_y"] = bcdim_y + 1
+            result = source_iteration(xs_total, xs_scatter, xs_fission, \
+                        external, boundary_x, boundary_y.flatten(), medium_map, \
+                        delta_x, delta_y, angle_x, angle_y, angle_w, info)
             boundary_y *= 0.0
             if loc == 1:
                 flux.append(result[:,::-1,0].flatten())
@@ -140,58 +138,25 @@ def test_dimensions_boundary_y():
 
 @pytest.mark.cylinder2d
 @pytest.mark.dimensions
-def test_dimensions_external_zero():
-    radii = [(0.0, 4.279960)]
-    radius = max(radii)[1]
-    center = (radius, radius)
-    cells_x = 100
-    cells_y = 100
-    delta_x = np.repeat(radius * 2 / cells_x, cells_x)
-    delta_y = np.repeat(radius * 2 / cells_y, cells_y)
-    # Cross Sections
-    xs_total = np.array([[0.32640], [0.0]])
-    xs_scatter = np.array([[[0.225216]], [[0.0]]])
-    xs_fission = np.array([[[2.84*0.0816]], [[0.0]]])
-    # Update cross sections for cylinder
-    # weight_map = math.monte_carlo_weight_matrix(delta_x, delta_y, \
-    #                                             center, radii)
-    weight_map = np.load("data/weight_map_cylinder_two_material.npy")
-    data = dimensions.cylinder_cross_sections(weight_map, xs_total, \
-                                              xs_scatter, xs_fission, \
-                                              cells_x, cells_y)
-    medium_map, xs_total, xs_scatter, xs_fission = data
-    medium_map = dimensions.expand_cylinder_medium_map(medium_map, [1,2,3,4])
-    # Material Parameters
-    groups = 1
-    angles = 4
-    bc = [0, 0]
-    # angles, angle_x, angle_y, angle_w = ants.calculate_xy_angles(angles, [bc,bc])
-    velocity = np.ones((groups))
-    external = np.zeros((cells_x, cells_y)).flatten()
-    # Set boundary terms
-    boundary_x = np.zeros((2,))
-    boundary_x[0] = 1.0
-    boundary_y = np.zeros((2,))
-    params = {"cells_x": cells_x, "cells_y": cells_y, "angles": angles, \
-             "groups": groups, "materials": len(xs_total), "geometry": 1, \
-             "spatial": 2, "qdim": 1, "bc_x": bc, "bcdim_x": 0, \
-             "bc_y": bc, "bcdim_y": 0, "steps": 0, "dt": 0, \
-             "adjoint": False, "angular": False}
-    angle_x, angle_y, angle_w = ants._angle_xy(params)
-    angles = params["angles"]
-    externals = [np.zeros((cells_x, cells_y)), \
-                 np.zeros((cells_x, cells_y, groups)), \
-                 np.zeros((cells_x, cells_y, angles, groups))]
+def test_dimensions_external_unit():
+    # Call original problem
+    xs_total, xs_scatter, xs_fission, medium_map, delta_x, delta_y, \
+        angle_x, angle_y, angle_w, info = cylinder_01()
+    # Set boundary_x and boundary_y
+    boundary_x = np.zeros((2))
+    info["bcdim_x"] = 1
+    boundary_y = np.zeros((2))
+    info["bcdim_y"] = 1
+    # Different boundary_x dimensions
+    externals = [np.ones((info["cells_x"] * info["cells_y"])), \
+                 np.ones((info["cells_x"] * info["cells_y"] * info["groups"])), \
+                 np.ones((info["cells_x"] * info["cells_y"] * info["angles"]**2 * info["groups"]))]
     flux = []
     for qdim, external in enumerate(externals):
-        params["qdim"] = qdim + 1
-        result = iteration(xs_total, xs_scatter, xs_fission, \
-                           external.flatten(), boundary_x, boundary_y, \
-                           medium_map.flatten(), delta_x, delta_y, \
-                           angle_x, angle_y, angle_w, params)
+        info["qdim"] = qdim + 1
+        result = source_iteration(xs_total, xs_scatter, xs_fission, \
+                    external, boundary_x, boundary_y, medium_map, \
+                    delta_x, delta_y, angle_x, angle_y, angle_w, info)
         flux.append(result[:,:,0].flatten())
     for ii in range(len(flux) - 1):
         assert np.all(np.isclose(flux[ii], flux[ii+1], atol=1e-10))
-
-# if __name__ == "__main__":
-#     test_dimensions_boundary_x()
